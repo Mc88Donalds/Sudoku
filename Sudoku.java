3702595072c21e5ccaabc7eb67bcb26dd1f8d1c1
Mc88Donalds <Mc88Donalds@users.noreply.github.com>
2020-02-27 20:07:39 +0100

    optimized generate algorithm, added more comments

--- Sudoku.java
+++ Sudoku.java
@@ -9,21 +9,18 @@ public class Sudoku {
 
     public Sudoku() {
         cells = new int[9][9];
-        generate(cells);
+        generate(cells, 0, 0);
         unsolve();
     }
 
     //is the grid done?
     public boolean done(int[][] grid) {
         //iterate over all cells
-        for (int[] row : grid
-        ) {
-            for (int cell : row
-            ) {
-                if (cell == 0)
+        for (int i = 0; i < grid.length; i++) {
+            for (int o = 0; o < grid[0].length; o++) {
+                if (grid[i][o] == 0)
                     return false;
             }
-
         }
         //if no cell equals 0 its done
         return true;
@@ -93,81 +90,79 @@ public class Sudoku {
         }
     }
 
-    public boolean generate(int[][] grid) {
-        for (int row = 0; ; row++) {
-            int col = 0;
-            //skip all done cells
-            while (col < grid[0].length && grid[row][col] != 0)
-                col++;
-            if (col >= grid[0].length)
-                continue;
+    //generate a new Cell
+    public boolean generate(int[][] grid, int row, int col) {
+        //randomise values
+        ArrayList<Integer> numbers = new ArrayList<>();
+        for (int i = 1; i < 10; i++)
+            numbers.add(i);
+        Collections.shuffle(numbers);
 
-            //randomise values
-            ArrayList<Integer> numbers = new ArrayList<Integer>();
-            for (int i = 1; i < 10; i++)
-                numbers.add(i);
-            Collections.shuffle(numbers);
+        //iterate over all randomised values
+        for (int numi = 0; numi < grid.length; numi++) {
+            int value = numbers.get(numi);
 
-            //iterate over all randomised values
-            for (int numi = 0; numi < grid.length; numi++) {
-                int value = numbers.get(numi);
-
-                //check if this value has been used in this row
-                boolean used = false;
-                for (int i = 0; i < grid.length; i++) {
-                    if (grid[row][i] == value) {
-                        used = true;
-                        break;
-                    }
-                }
-                if (used)
-                    continue;
-
-                //check if this value has been used in this column
-                for (int i = 0; i < grid.length; i++) {
-                    if (grid[i][col] == value) {
-                        used = true;
-                        break;
-                    }
-                }
-                if (used)
-                    continue;
-
-                //find the block
-                int x = (col / 3) * 3;
-                int y = (row / 3) * 3;
-
-                //check the block
-                for (int i = 0; i < 3; i++) {
-                    for (int o = 0; o < 3; o++) {
-                        if (grid[i + y][o + x] == value) {
-                            used = true;
-                            break;
-                        }
-                    }
-                }
-                if (used)
-                    continue;
-                //set value
-                grid[row][col] = value;
-
-                //solved grid
-                if (done(grid)) {
-                    System.out.println("Grid Done!");
-                    cells = grid;
-                    return true;
-                }
-                //next step
-                else if (generate(grid)) {
-                    return true;
+            //check if this value has been used in this row
+            boolean used = false;
+            for (int i = 0; i < grid.length; i++) {
+                if (grid[row][i] == value) {
+                    used = true;
+                    break;
                 }
             }
-            grid[row][col] = 0;
-            return false;
+            if (used)
+                continue;
+
+            //check if this value has been used in this column
+            for (int i = 0; i < grid.length; i++) {
+                if (grid[i][col] == value) {
+                    used = true;
+                    break;
+                }
+            }
+            if (used)
+                continue;
+
+            //find the block
+            int x = (col / 3) * 3;
+            int y = (row / 3) * 3;
+
+            //check the block
+            for (int i = 0; i < 3; i++) {
+                for (int o = 0; o < 3; o++) {
+                    if (grid[i + y][o + x] == value) {
+                        used = true;
+                        break;
+                    }
+                }
+            }
+            if (used)
+                continue;
+            //set value
+            grid[row][col] = value;
+
+            col++;
+            if (col == 9) {
+                col = 0;
+                row++;
+            }
+
+            //solved grid
+            if (done(grid)) {
+                System.out.println("Grid Done!");
+                cells = grid;
+                return true;
+            }
+            //next step
+            else if (generate(grid, row, col)) {
+                return true;
+            }
         }
+        grid[row][col] = 0;
+        return false;
     }
 
-
+    //remove cells
     public void unsolve() {
         //random position
         int x = (int) (Math.random() * 9);
@@ -191,9 +186,11 @@ public class Sudoku {
         solutions = 0;
         solutions(copy);
 
-        //undo
         if (solutions != 1) {
+            //undo deletion
             cells[x][y] = value;
+
+            //check whole grid
             for (x = 0; x < cells.length; x++) {
                 for (y = 0; y < cells[0].length; y++) {
                     while (y < 9 && cells[x][y] == 0) {
@@ -212,6 +209,7 @@ public class Sudoku {
                     solutions = 0;
                     solutions(copy);
                     if (solutions != 1) {
+                        //undo
                         cells[x][y] = value;
                     }
                 }
